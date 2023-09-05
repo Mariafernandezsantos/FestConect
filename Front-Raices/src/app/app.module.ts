7f40456c15e3e36fc15768733093a88819372c07
María <133371373+Mariafernandezsantos@users.noreply.github.com>
2023-09-05 09:09:01 +0200

    intalacion nuevo paquete

--- Front-Raices/src/app/app.module.ts
+++ Front-Raices/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './shared/components/login/login.component';
 import { IconsComponent } from './shared/components/icons/icons.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { IconsComponent } from './shared/components/icons/icons.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
